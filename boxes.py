e585933ded09055479d68ec99a4b123729cf82ed
Amanda <amjustin@iupui.edu>
2016-11-24 03:21:47 -0500

    working GUI, added more graphic files, sound and simple functionality

--- boxes.py
+++ boxes.py
@@ -1,42 +1,60 @@
 import pygame
+import math
 
 class GomokuGame():
     def __init__(self):
         pass
-        #1
+
         pygame.init()
         pygame.font.init()
+        width, height = 580, 680
 
-        width, height = 520, 680
-
-        #2
         #initialize the screen
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Gomoku")
-        #3
-        #initialize pygame clock
         self.clock=pygame.time.Clock()
 
-        # define two arrays for placement of the Boxes
-        self.boardh = [[False for x in range(19)] for y in range(20)]
-        self.boardv = [[False for x in range(20)] for y in range(19)]
-
-        #initialize the graphics
+        # define array for placement of the boxes
+        self.board = [[False for x in range(19)] for y in range(19)]
         self.initGraphics()
+        self.initSound()
+        self.turn = True
+        self.owner=[[0 for x in range(6)] for y in range(6)]
+        self.me=0
+        self.otherplayer=0
+        self.didiwin=False
+        self.running=False
 
+    # --------------------------------
+    # Build and update the board!
+    # --------------------------------
     def drawBoard(self):
         for x in range(19):
-            for y in range(20):
-                if not self.boardh[y][x]:
-                    self.screen.blit(self.normallineh, [(x)*25+5, (y)*27])
-                else:
-                    self.screen.blit(self.bar_doneh, [(x)*25+5, (y)*27])
-        for x in range(20):
             for y in range(19):
-                if not self.boardv[y][x]:
-                    self.screen.blit(self.normallinev, [(x)*27, (y)*25+5])
-                else:
-                    self.screen.blit(self.bar_donev, [(x)*27, (y)*25+5])
+                if not self.board[y][x]:
+                    self.screen.blit(self.bluesquare, [(x)*30, (y)*30+5])
+    def drawHUD(self):
+        #draw the background for the bottom and draw text:
+        self.screen.blit(self.scorepanel, [0, 580])
+
+        #define fonts
+        myfont32 = pygame.font.SysFont(None, 32)
+        myfont64 = pygame.font.SysFont(None, 64)
+        myfont20 = pygame.font.SysFont(None, 20)
+
+        label = myfont32.render("Your Turn:", 1, (255,255,255))
+        scoreme = myfont64.render(str(self.me), 1, (255,255,255))
+        scoreother = myfont64.render(str(self.otherplayer), 1, (255,255,255))
+        scoretextme = myfont20.render("You", 1, (255,255,255))
+        scoretextother = myfont20.render("Other Player", 1, (255,255,255))
+
+        #draw surface
+        self.screen.blit(label, (10, 590))
+        self.screen.blit(self.greenindicator if self.turn else self.redindicator, (130, 585))
+        self.screen.blit(scoretextme, (10, 625))
+        self.screen.blit(scoreme, (10, 635))
+        self.screen.blit(scoretextother, (280, 625))
+        self.screen.blit(scoreother, (340, 635))
 
     def update(self):
         #sleep to make the game 60 fps
@@ -47,6 +65,7 @@ class GomokuGame():
 
         #draw the board
         self.drawBoard()
+        self.drawHUD()
 
         for event in pygame.event.get():
             #quit if the quit button was pressed
@@ -56,14 +75,25 @@ class GomokuGame():
         #update the screen
         pygame.display.flip()
 
-    def initGraphics(self):
-        self.normallinev=pygame.image.load("normalline.png")
-        self.normallineh=pygame.transform.rotate(pygame.image.load("normalline.png"), -90)
-        self.bar_donev=pygame.image.load("bar_done.png")
-        self.bar_doneh=pygame.transform.rotate(pygame.image.load("bar_done.png"), -90)
-        self.hoverlinev=pygame.image.load("hoverline.png")
-        self.hoverlineh=pygame.transform.rotate(pygame.image.load("hoverline.png"), -90)
+    # --------------------------------
+    # Initialize Graphics and Sounds!
+    # --------------------------------
+    def initSound(self):
+        pygame.mixer.music.load("music.wav")
+        self.winSound = pygame.mixer.Sound('win.wav')
+        self.loseSound = pygame.mixer.Sound('lose.wav')
+        self.placeSound = pygame.mixer.Sound('place.wav')
+        pygame.mixer.music.play()
 
+    def initGraphics(self):
+        self.bluesquare=pygame.image.load("square-frame-png-25171.png")
+        self.blackcircle=pygame.image.load("blackcircle.png")
+        self.bluecircle=pygame.image.load("bluecircle.png")
+        self.orangecircle=pygame.image.load("orangecircle.png")
+        self.redcircle=pygame.image.load("redcircle.png")
+        self.scorepanel=pygame.image.load("score_panel.png")
+        self.greenindicator=pygame.image.load("greenindicator.png")
+        self.redindicator=pygame.image.load("redindicator.png")
 
 bg=GomokuGame() #__init__ is called right here
 while 1:
